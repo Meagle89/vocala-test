e285de014c2f0536464cf4deab0a04110e338b94
Meagle89 <74543657+Meagle89@users.noreply.github.com>
2021-07-05 10:42:47 +0100

    modified code

--- libs/php/config.php
+++ libs/php/config.php
@@ -3,8 +3,8 @@
 	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root"; // user name
-	$cd_password = "Be@gle1989"; // password
+	$cd_user = "****"; // user name
+	$cd_password = "****"; // password
 	$cd_dbname = "contacts"; // database name
 
 ?>
